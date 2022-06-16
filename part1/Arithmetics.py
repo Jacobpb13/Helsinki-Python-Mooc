3d8fede8c19474508965d434df8b1206a3b3ce18
Jacob Pembroke-Burn <jacobpb13@live.co.uk>
2022-06-16 17:55:29 +0100

    Part one-more about variables

---
+++ part1/Arithmetics.py
@@ -0,0 +1,18 @@
+# Programming exercise:
+# Arithmetics
+# Please complete the program so that it also prints out the following:
+
+# Sample output
+# 27 + 15 = 42
+# 27 - 15 = 12
+# 27 * 15 = 405
+# 27 / 15 = 1.8
+
+# Write your solution here
+x = 27
+y = 15
+
+print(x,"+",y, "=" ,x+y)
+print(x,"-",y, "=" ,x-y)
+print(x,"*",y, "=" ,x*y)
+print(x,"/",y, "=" ,x/y)
